e65181c95c90333fe909931d590fb4870b2e2691
Chen <chenzalman96@gmail.com>
2024-11-22 17:37:31 +0200

    Upload project to Github
    This is the first commit to upload the project to save it on my github.

---
+++ app/src/main/java/com/example/restaurant_ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.restaurant_ui
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val dateBtn: Button = findViewById<Button>(R.id.date_picker_button)
+
+        dateBtn.setOnClickListener{
+        }
+    }
+}
