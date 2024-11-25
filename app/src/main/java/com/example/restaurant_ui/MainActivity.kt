e365fcf79abeece1749c3dcce75e2e086aa08fd0
Chen <chenzalman96@gmail.com>
2024-11-25 15:15:03 +0200

    Addid animations
    Added animations and seperate the main function even more to reduse load and cleaner code

--- app/src/main/java/com/example/restaurant_ui/MainActivity.kt
+++ app/src/main/java/com/example/restaurant_ui/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.restaurant_ui
 
-import android.annotation.SuppressLint
-import android.app.Activity
+
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
-import android.content.res.Resources
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +29,41 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        //All of the widgets and variables declaration
         val dateBtn: Button = findViewById(R.id.date_picker_button)
         val dateText: TextView = findViewById(R.id.date_text)
         val timeText: TextView = findViewById(R.id.time_text)
         val spinner: Spinner = findViewById(R.id.vegan_spinner)
         val veganText: TextView = findViewById(R.id.vegan_id)
         val reserveBtn: Button = findViewById(R.id.reserve_btn)
+        val numDiners: EditText = findViewById(R.id.num_diners)
+        var paymentMethod: String? = null
+        val radioGroup: RadioGroup = findViewById(R.id.radio_group)
+        val linearView: LinearLayout = findViewById(R.id.linearLayoutMain)
+        val enlargeAnim = AnimationUtils.loadAnimation(applicationContext, R.anim.enlarge)
+
+        //Function calls to distribute the load from the main function
+        linearView.startAnimation(enlargeAnim)
+        spinnerListener(spinner, veganText)
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            paymentMethod = changePaymentOption(radioGroup = group)
+        }
 
         dateBtn.setOnClickListener {
             dateAndTime(dateText, timeText, this)
         }
+        reserveBtn.setOnClickListener {
+            dialogCreator(
+                numDiners,
+                dateText,
+                timeText,
+                veganText,
+                paymentMethod
+            )
+        }
 
-        // Create an ArrayAdapter using the string array and a default spinner layout.
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
             R.array.vegan_options,
@@ -47,56 +74,101 @@ class MainActivity : AppCompatActivity() {
             // Apply the adapter to the spinner.
             spinner.adapter = adapter
         }
-        spinnerListener(spinner,veganText)
-//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-//                // An item is selected. You can retrieve the selected item using
-//                veganText.text = parent.getItemAtPosition(pos).toString()
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {
-//                veganText.text = getString(R.string.no_one_is_vegan)
-//
-//            }
-//        }
     }
-}
 
-fun dateAndTime(dateText: TextView, timeText: TextView, context: Context): Unit {
-    val calendar = Calendar.getInstance()
-    val listener = DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
-        dateText.text = ("$i.$i2.$i3")
+    //This function sets the payment method to the paymentMethod String.
+    fun changePaymentOption(radioGroup: RadioGroup): String? {
+        var paymentMethod: String? = null
+        if (radioGroup.findViewById<RadioButton?>(R.id.cash).isChecked)
+            paymentMethod = radioGroup.findViewById<RadioButton?>(R.id.cash).text.toString()
+
+        if (radioGroup.findViewById<RadioButton?>(R.id.credit_card).isChecked)
+            paymentMethod = radioGroup.findViewById<RadioButton?>(R.id.credit_card).text.toString()
+
+        if (radioGroup.findViewById<RadioButton?>(R.id.payment_app).isChecked)
+            paymentMethod = radioGroup.findViewById<RadioButton?>(R.id.payment_app).text.toString()
+        Toast.makeText(this, "Payment method changed to: $paymentMethod", Toast.LENGTH_SHORT).show()
+        return paymentMethod
     }
-    val dtd = DatePickerDialog(
-        context, //this,
-        listener,
-        calendar.get(Calendar.YEAR),
-        calendar.get(Calendar.MONTH),
-        calendar.get(Calendar.DAY_OF_MONTH)
-    )
-    dtd.show()
 
-
-    val time = Calendar.getInstance().time
-    val timeListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-        timeText.text = ("$hourOfDay:$minute")
+    //This function creates a custom dialog to display multiple lines of text to create a summary of the reservation and display and option to return or finish the program (alternative to send a reservation)
+    fun dialogCreator(
+        numDiners: EditText,
+        dateText: TextView,
+        timeText: TextView,
+        veganText: TextView,
+        paymentMethod: String? = null
+    ): Unit {
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_layout, null)
+        builder.setView(dialogView)
+        val dialog = builder.create()
+        dialog.setCancelable(false)
+        dialogView.findViewById<TextView>(R.id.textViewNumberOfDiners).text =
+            getString(R.string.number_of_diners) + ": ${numDiners.text}"
+        dialogView.findViewById<TextView>(R.id.textViewPaymentMethod).text =
+            getString(R.string.payment_method) + " ${paymentMethod}"
+        dialogView.findViewById<TextView>(R.id.textViewDateOfReservation).text =
+            getString(R.string.date_reserved_at, dateText.text, timeText.text)
+        dialogView.findViewById<TextView>(R.id.textViewAnyVegans).text =
+            getString(R.string.any_vegans) + " ${veganText.text}"
+        dialogView.findViewById<Button>(R.id.buttonReserve).setOnClickListener {
+            Toast.makeText(this, getString(R.string.reserve_sits), Toast.LENGTH_LONG).show()
+            finish()
+        }
+        dialogView.findViewById<Button>(R.id.buttonEdit).setOnClickListener {
+            Toast.makeText(this, getString(R.string.edit_and_return), Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
-    val ttd = TimePickerDialog(context, timeListener, Calendar.HOUR, Calendar.MINUTE, true)
-    ttd.show()
-}
 
-fun spinnerListener(spinner: Spinner, veganText: TextView): Unit{
+    //This function set the selected option from the spinner (dropdown) menu to the text
+    fun spinnerListener(
+        spinner: Spinner,
+        veganText: TextView
+    ): Unit {
 
-    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-            // An item is selected. You can retrieve the selected item using
-            veganText.text = parent.getItemAtPosition(pos).toString()
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+                // An item is selected. You can retrieve the selected item using
+                veganText.text = parent.getItemAtPosition(pos).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                veganText.text = getString(R.string.no_one_is_vegan)
+            }
         }
 
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-            veganText.text = Resources.getSystem().getString(R.string.no_one_is_vegan)
-
-        }
     }
 
+    //This function create the time dialog and then creates the date dialog and display the date dialog above the time dialog
+    fun dateAndTime(
+        dateText: TextView,
+        timeText: TextView,
+        context: Context
+    ): Unit {
+        val calendar = Calendar.getInstance()
+
+        val timeListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            timeText.text = ("$hourOfDay:$minute")
+        }
+        val tpd = TimePickerDialog(context, timeListener, Calendar.HOUR, Calendar.MINUTE, true)
+        tpd.show()
+
+        val listener = DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+            dateText.text = ("$i.$i2.$i3")
+        }
+        val dpd = DatePickerDialog(
+            context, //this,
+            listener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        dpd.show()
+    }
 }
+
+
+
